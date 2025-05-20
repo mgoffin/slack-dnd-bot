e00d43eaa2293ebc313d6f696ca08a53dfa72993
Mike Goffin <mgoffin@gmail.com>
2025-05-20 08:46:49 -0400

    Update dnd-bot.py
    Updated to use flags for argument parsing.

--- dnd-bot.py
+++ dnd-bot.py
@@ -1,6 +1,10 @@
+import argparse
+import html
 import json
 import os
 import requests
+import shlex
+import urllib.parse
 
 from d20 import roll
 from flask import abort, Flask, request
@@ -13,31 +17,31 @@ response_type = "in_channel"  # ephemeral
 
 # Character lookup for anyone with a real name and image.
 # Each key/value pair should be in the form of:
-# 'slash_cmd': {'name': 'Display Name', 'image': 'url to image to display'}
+# 'command_name': {'name': 'Display Name', 'image': 'url to image to display'}
 c_map = {
-    'character_name': {
-        'name': "Display Name",
-        'image': "URI to character image.",  # noqa:E501
+    'drizzt': {
+        'name': "Drizzt Do'Urden",
+        'image': "https://en.wikipedia.org/wiki/Drizzt_Do%27Urden#/media/File:Drizzt.png",  # noqa:E501
     },
 }
 
 
-def get_image(character):
+def get_image(inputs):
     '''Get the image of a given character in the c_map'''
 
     try:
-        return c_map[character.lower()]['image']
+        return c_map[inputs.character.lower()]['image']
     except Exception:
         return None
 
 
-def get_name(character):
+def get_name(inputs):
     '''Get the name of a given character in the c_map'''
 
     try:
-        return c_map[character.lower()]['name']
+        return c_map[inputs.character.lower()]['name']
     except Exception:
-        return character
+        return inputs.character
 
 
 def is_request_valid(request):
@@ -49,38 +53,69 @@ def is_request_valid(request):
     return token and team_id
 
 
-def parse_gm_command(request):
-    '''Parse a message as a GM command.'''
+def parse_command(request):
+    '''Parse the message as a command.'''
 
-    msg_l = request.form['text'].split('|')
-    if len(msg_l) == 3:
-        character = msg_l[0].strip()
-        mode = " [{}]".format(msg_l[1].strip())
-        msg = msg_l[2].strip()
-    elif len(msg_l) == 2:
-        character = msg_l[0].strip()
-        mode = ""
-        msg = msg_l[1].strip()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c',
+        '--character',
+        action='store',
+    )
+    parser.add_argument(
+        '-e',
+        '--emotion',
+        action='store',
+        default='',
+    )
+    parser.add_argument(
+        '-H',
+        '--how',
+        action='store_true',
+        default=False,
+    )
+    parser.add_argument(
+        '-m',
+        '--message',
+        action='store',
+        default='',
+    )
+    parser.add_argument(
+        '-r',
+        '--roll',
+        action='store',
+        default=None,
+    )
 
-    return character, mode, msg
+    # This sucks:
+    # The characters coming from different Slack clients can be unicode or not
+    # In our situatio, iOS sends unicode quotes (at the very least) which
+    # shlex chokes on.
+    # Found this in stackoverflow which basically is a translation table from
+    # silly unicode characters to associated ascii characters.
+    # This list will need to be expanded over time if we find more unicode
+    # characters that mess things up.
+    # We can use `zappa tail` to check the traceback and see what's messing up
+    # ...which I might leave the print statements here to facilitate easier
+    # debugging
+    transl_table = dict(
+        [(ord(x), ord(y)) for x, y in zip(u"‘’´“”–-",  u"'''\"\"--")]
+    )
+    try:
+        url_decoded = urllib.parse.unquote(request.form['text'])
+        html_decoded = html.unescape(url_decoded)
+        fixed_str = html_decoded.translate(transl_table)
+        shlexd = shlex.split(fixed_str)
+        args = parser.parse_args(shlexd)
+    except Exception as e:
+        print(f'Fixed String: {fixed_str}')
+        print(str(e))
+        abort(400)
+
+    return args
 
 
-def parse_character_command(request):
-    '''Parse a message as a Player command.'''
-
-    msg_l = request.form['text'].split('|')
-    character = request.form['command'].replace("/", "")
-    if len(msg_l) == 2:
-        mode = " [{}] ".format(msg_l[0].strip())
-        msg = msg_l[1].strip()
-    else:
-        mode = ""
-        msg = msg_l[0]
-
-    return character, mode, msg
-
-
-def generate_accessory(character):
+def generate_accessory(inputs):
     '''Generate the Block accessory for character images.
 
     If a character is provided that has no image, set to None.
@@ -88,7 +123,7 @@ def generate_accessory(character):
 
     accessory = {
         "type": "image",
-        "image_url": get_image(character),
+        "image_url": get_image(inputs),
         "alt_text": "Characte Image"
     }
     if accessory['image_url'] is None:
@@ -97,17 +132,34 @@ def generate_accessory(character):
     return accessory
 
 
-def generate_text(character, mode, user, msg):
+def generate_text(user, inputs):
     '''Generate the text to be sent to Slack.'''
 
+    if inputs.how:
+        hlp = (
+            "/char -c [character name]\n"
+            "If you'd like to add an emotion or action: -e '[emotion|action]'\n"  # noqa:E501
+            "If you'd like to show what the characer says: -m \"[text]\"\n"
+            "If you want to roll dice as well: -r \"[roll]\"\n"
+        )
+        return "{}".format(hlp)
+
     # Special mode checks go here if you want to do something special.
-    if mode.strip() == '[roll]':
-        result = str(roll(msg))
-        return "*{} Rolls... (from {})*\n{}".format(
-                get_name(character), user, result)
+    if inputs.roll:
+        result = f'{inputs.message}\n\n{str(roll(inputs.roll))}'
+        return "*{} ...{} (from {})*\n{}".format(
+                    get_name(inputs),
+                    inputs.emotion,
+                    user,
+                    result
+                )
     else:
-        return "*{} Says...{} (from {})*\n{}".format(
-                get_name(character), mode, user, msg)
+        return "*{} ...{} (from {})*\n{}".format(
+                    get_name(inputs),
+                    inputs.emotion,
+                    user,
+                    inputs.message
+                )
 
 
 def build_data(channel, text, accessory):
@@ -152,33 +204,25 @@ def post_to_slack(response_url, data):
     )
 
 
-def generate_message(request, allowed=[]):
+def generate_message(request):
     '''Generate the message to send to Slack'''
 
     if not is_request_valid(request):
         abort(400)
 
-    command = request.form['command']
     channel = request.form['channel_id']
     response_url = request.form['response_url']
     user = request.form['user_name']
 
-    # Lock down this particular message to specific users.
-    if user not in allowed and 'all' not in allowed:
-        abort(400)
-
-    # GM Command is more flexible in the form of `/gm name | message`
-    if command == "/gm":
-        character, mode, msg = parse_gm_command(request)
-    else:
-        character, mode, msg = parse_character_command(request)
+    # Parse the command to get inputs
+    inputs = parse_command(request)
 
     # If we don't have an image, don't include it in the accessory or the
     # POST to Slack will be invalid and fail.
-    accessory = generate_accessory(character)
+    accessory = generate_accessory(inputs)
 
     # Generate message text.
-    text = generate_text(character, mode, user, msg)
+    text = generate_text(user, inputs)
 
     # POST data to Slack using a Block (made using Block Builder)
     data = build_data(channel, text, accessory)
@@ -197,14 +241,6 @@ def generate_message(request, allowed=[]):
 # Each one of these is a command we can use in Slack with `/foo`
 
 
-@app.route('/character', methods=['POST'])
-def character():
-    '''App route for this character's slash command.
-
-    You need to specify the slack username that is allowed to speak as this
-    character. You can also specify 'all' if you wish to allow anyone to speak
-    as this character.
-    '''
-
-    allowed = ['slack_username']
-    return generate_message(request, allowed=allowed)
+@app.route('/char', methods=['POST'])
+def char():
+    return generate_message(request)
